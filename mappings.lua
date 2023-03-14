a8fc116e4127adbbd09d30a3716ce4a40d778bd4
Federico Blancato <federico.blancato@infinitaslearning.com>
2023-03-14 12:05:13 +0100

    Tokyonight and escesc mapping

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,10 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
@@ -22,6 +25,7 @@ return {
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
   t = {
+    ["<Esc><Esc>"] = { "<C-\\><C-n>" },
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },

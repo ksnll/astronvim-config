39b25b6249704d93b233f207b05b20393cb18d88
Federico Blancato <kaosone@gmail.com>
2024-05-02 19:20:36 +0300

    Astronvim v4

--- lua/community.lua
+++ lua/community.lua
@@ -1,9 +1,6 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.rust" },
